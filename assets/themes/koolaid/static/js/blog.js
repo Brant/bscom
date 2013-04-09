d648b84cfa9ea2f22a3378538b1c54126bf6bbb5
Brant Steen <brant.steen@gmail.com>
2013-04-08 21:53:53 -0400

    This doesnt work anyway...

--- assets/themes/koolaid/static/js/blog.js
+++ assets/themes/koolaid/static/js/blog.js
@@ -1,8 +1,4 @@
 $(function(){
-	if ($("body").hasClass("blog-index")){
-		$("#content").jaxy({"fromBottom": 500});		
-	}
-	
 	if ($("body").hasClass("blog-single")){
 		hljs.tabReplace = "  ";
 		hljs.initHighlightingOnLoad();
