4e90cf1d631c4730c1a7def7d08e12ed2c75430d
Brant Steen <brant.steen@gmail.com>
2014-04-29 18:31:12 -0400

    Some blog updates (admin, primarily)

--- bscom/blog/admin.py
+++ bscom/blog/admin.py
@@ -1,13 +1,38 @@
+"""
+Blog admin configurations
+"""
 from django.contrib import admin
 
 from bscom.blog.models import Entry, Category
+
+
+def reslug(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.slug = None
+        obj.save()
+reslug.short_description = "Re-slug"
+
 
 class EntryAdmin(admin.ModelAdmin):
     """
     Blog Entry admin configuration
     """
-    list_display = ["title", "date", "category"]
+    actions = [reslug, ]
+    fieldsets = (
+        (None, {
+            "fields": ["title", "slug", "content", "blurb", "thumbnail", ]
+        }),
+        ("For Links", {
+            "fields": ["external_link"]
+        }),
+        ("Metadata", {
+            "fields": ["category", "tags", ]
+        })
+    )
+
+    list_display = ["title", "slug", "date", "category"]
     list_filter = ["category"]
-    
+    readonly_fields = ["slug", ]
+
 admin.site.register(Category)
 admin.site.register(Entry, EntryAdmin)
