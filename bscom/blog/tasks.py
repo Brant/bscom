9ea5008363033caef1109387b1ebd922e2d63d92
Brant Steen <brant.steen@gmail.com>
2014-07-04 16:53:12 -0400

    looking

--- bscom/blog/tasks.py
+++ bscom/blog/tasks.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import hmac
 import hashlib
 import base64
+import dropbox
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -16,13 +17,12 @@ from celery import shared_task
 def import_drafts_from_dropbox(request):
     with open("/home/brant/bscom16/task.log", "a") as f:
         f.write("Updated... %s\n" % datetime.now())
-        f.write("%s\n" % request.body)
+        # f.write("%s\n" % request.body)
 
-        f.write("%s\n" % request.META['HTTP_X_DROPBOX_SIGNATURE'])
-
-        # message = bytes(request.body).encode('utf-8')
-        # secret = bytes(settings.DROPBOX_SECRET).encode('utf-8')
+        # f.write("%s\n" % request.META['HTTP_X_DROPBOX_SIGNATURE'])
 
         signature = hmac.new(settings.DROPBOX_SECRET, request.body, digestmod=hashlib.sha256).hexdigest()
-        f.write("%s\n" % signature)
+        # f.write("%s\n" % signature)
 
+        client = dropbox.client.DropboxClient(settings.DROPBOX_TOKEN)
+        f.write("%s\n" % client.metadata(settings.DROPBOX_DRAFTS_PATH))
