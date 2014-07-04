88750e46ed2b26b28479bc939e558fc42595818b
Brant Steen <brant.steen@gmail.com>
2014-07-04 16:05:23 -0400

    hmm??

--- bscom/blog/views.py
+++ bscom/blog/views.py
@@ -9,6 +9,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_exempt
 
 from bscom.blog.models import Entry, Category
 from bscom.blog.tasks import import_drafts_from_dropbox
@@ -17,6 +18,7 @@ from noodles.util import make_paginator
 from noodles.views import json_response
 
 
+@csrf_exempt
 @never_cache
 def import_drafts(request):
     challenge = request.GET.get("challenge", None)
