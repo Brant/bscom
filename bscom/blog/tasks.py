82e09bd0e68ae99fdf88c9cb7dac6413b3e19526
Brant Steen <brant.steen@gmail.com>
2014-07-04 16:29:03 -0400

    verifying

--- bscom/blog/tasks.py
+++ bscom/blog/tasks.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+import hmac
+import hashlib
+import base64
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -13,3 +17,7 @@ def import_drafts_from_dropbox(request):
     with open("/home/brant/task.log", "a") as f:
         f.write("Updated... %s\n" % datetime.now())
         f.write("%s" % request.body)
+        f.write("%s" % request.META['X-Dropbox-Signature'])
+        dig = hmac.new(settings.DROPBOX_SECRET, msg=request.body, digestmod=hashlib.sha256).digest()
+        f.write("%s" % dig)
+        # base64.b64encode(dig).decode()
