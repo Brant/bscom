a3a5f922ae27a79cf49832ef11e3d99e21e6e04a
Brant Steen <brant.steen@gmail.com>
2014-07-04 16:36:38 -0400

    annoying

--- bscom/blog/tasks.py
+++ bscom/blog/tasks.py
@@ -14,7 +14,7 @@ from celery import shared_task
 
 @shared_task
 def import_drafts_from_dropbox(request):
-    with open("/home/brant/task.log", "a") as f:
+    with open("/home/brant/bscom16/task.log", "a") as f:
         f.write("Updated... %s\n" % datetime.now())
         f.write("%s\n" % request.body)
 
