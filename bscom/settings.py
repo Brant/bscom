5a6db369a9efe6bc1b424e1375f4f28ddafae62b
Brant Steen <brant.steen@gmail.com>
2014-07-04 10:15:31 -0400

    remove end on mediaroot

--- bscom/settings.py
+++ bscom/settings.py
@@ -60,7 +60,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = PROJECT_PATH + "/assets/uploads/"
+MEDIA_ROOT = PROJECT_PATH + "/assets/uploads"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
