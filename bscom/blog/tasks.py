8aee1117a76a7e2ec0574ac745be166c8f96e8d0
Brant Steen <brant.steen@gmail.com>
2014-07-04 21:59:12 -0400

    save it all!

--- bscom/blog/tasks.py
+++ bscom/blog/tasks.py
@@ -32,7 +32,7 @@ def import_drafts_from_dropbox(request):
         while has_more:
             result = client.delta(None)
             for path, metadata in result['entries']:
-                if path.startswith(settings.DROPBOX_DRAFTS_PATH):
+                # if path.startswith(settings.DROPBOX_DRAFTS_PATH):
                     f.write("%s\n" % path)
                     # f.write("%s\n" % metadata)
             has_more = result['has_more']
