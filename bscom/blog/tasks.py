8d951aa0755e26fb3b5e3a45f2452dfdf2017006
Brant Steen <brant.steen@gmail.com>
2014-07-06 17:53:55 -0400

    some cleanup

--- bscom/blog/tasks.py
+++ bscom/blog/tasks.py
@@ -1,17 +1,12 @@
 from __future__ import absolute_import
 
-from datetime import datetime
 from dateutil import parser
 
-import hmac
-import hashlib
-import base64
 import dropbox
 
 from django.conf import settings
-from django.contrib.auth.models import User
+
 from django.core.exceptions import ObjectDoesNotExist
-from django.utils.timezone import utc
 
 from celery import shared_task
 
@@ -22,54 +17,25 @@ from bscom.blog.models import Entry
 
 @shared_task
 def import_drafts_from_dropbox():
-
+    """
+    Imports .md files from a specified settings.DROPBOX_DRAFTS_PATH
+    """
     client = dropbox.client.DropboxClient(settings.DROPBOX_TOKEN)
 
     for item in client.metadata(settings.DROPBOX_DRAFTS_PATH)['contents']:
-        mod_time = parser.parse(item['modified'])
+        if item['path'].endswith(".md"):
+            mod_time = parser.parse(item['modified'])
 
-        try:
-            entry = Entry.objects.get(draft_file=item['path'])
-        except ObjectDoesNotExist:
-            entry = Entry(draft_file=item['path'], draft=True)
+            try:
+                entry = Entry.objects.get(draft_file=item['path'])
+            except ObjectDoesNotExist:
+                entry = Entry(draft_file=item['path'], draft=True)
 
-        if entry.draft and (not entry.date_modified or entry.date_modified < mod_time):
-            f, metadata = client.get_file_and_metadata(item['path'])
-            lines = f.readlines()
-            title = lines[0].strip().replace("#", "")
-            body = "".join(lines[1:])
-            entry.content = body.strip()
-            entry.title = title
-            entry.save()
-
-
-
-        # f, metadata = client.get_file_and_metadata(item['path'])
-        # lines = f.readlines()
-        # print lines
-
-    # with open("/home/brant/bscom16/task.log", "a") as f:
-    #     f.write("Updated... %s\n" % datetime.utcnow().replace(tzinfo=utc))
-    #     f.write("%s\n" % client.metadata(settings.DROPBOX_DRAFTS_PATH))
-
-
-        # f.write("Updated... %s\n" % datetime.now())
-        # f.write("%s\n" % request.body)
-
-        # f.write("%s\n" % request.META['HTTP_X_DROPBOX_SIGNATURE'])
-        # f.write("%s\n" % signature)
-
-        #
-        # has_more = True
-
-        # while has_more:
-        #     result = client.delta(None)
-        #     for path, metadata in result['entries']:
-        #         # if path.startswith(settings.DROPBOX_DRAFTS_PATH):
-        #             f.write("%s\n" % path)
-        #             # f.write("%s\n" % metadata)
-        #     has_more = result['has_more']
-
-        # if (metadata is None or metadata['is_dir'] or not path.endswith('.md')):
-        # f.write("%s\n" % client.metadata(settings.DROPBOX_DRAFTS_PATH))
-
+            if entry.draft and (not entry.date_modified or entry.date_modified < mod_time):
+                f, metadata = client.get_file_and_metadata(item['path'])
+                lines = f.readlines()
+                title = lines[0].strip().replace("#", "")
+                body = "".join(lines[1:])
+                entry.content = body.strip()
+                entry.title = title
+                entry.save()
