d471564d4ea2a90c2e1ea10ae6662e32f425aba4
Brant Steen <brant.steen@gmail.com>
2014-08-03 10:25:57 -0400

    uwsgi for newrelic

--- bscom/wsgi.py
+++ bscom/wsgi.py
@@ -14,6 +14,10 @@ framework.
 
 """
 import os
+import newrelic.agent
+
+newrelic.agent.initialize('bucksac_config/newrelic.ini', "production")
+
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
@@ -25,8 +29,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bscom.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
